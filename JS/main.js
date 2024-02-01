3a306f6f754f0dafdff08d72d84c05ea1c461b77
Omkarkal <92387691+Omkarkal@users.noreply.github.com>
2024-02-01 11:55:45 +0530

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSiderbar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
